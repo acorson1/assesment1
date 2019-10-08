4e669f70f5f77f73c73b8f5309f877a0a2d28785
acorson1 <33963426+acorson1@users.noreply.github.com>
2019-10-08 14:30:41 +0100

    Add files via upload

---
+++ assessment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //alex corson formative assessment 1
+            Console.WriteLine("enter a whole number for the length of a cube side in cm");
+            string temp = Console.ReadLine();
+            int side = Convert.ToInt32(temp);
+            //this will request the user to type how long one of the edges of a cube is 
+
+            int volume = side * side * side;
+            Console.WriteLine(volume);
+            //used to calculate the volume where one of the edges is known by cubing that value
+            //and then wrtiing what that value is to the user
+        }
+    }
+}
